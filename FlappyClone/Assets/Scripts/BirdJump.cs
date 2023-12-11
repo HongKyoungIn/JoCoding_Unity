43b532f446d36ec48752ac18bfff4ae908cfef1e
HongKyoungIn <ghdruddls999@naver.com>
2023-12-11 22:00:26 +0900

    2023-12-11 Study : 점프 기능 구현

--- FlappyClone/Assets/Scripts/BirdJump.cs
+++ FlappyClone/Assets/Scripts/BirdJump.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class BirdJump : MonoBehaviour {
 
-    Rigidbody2D rb;
+    Rigidbody2D rb; // Rigidbody2D 자료형의 rb변수를 선언한다.
 
     void Start() {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); // rb변수 안에 Rigidbody2D 컴포넌트를 받아온다.
     }
 
 
     void Update() {
-        if(Input.GetMouseButtonDown(0)) {
-            rb.velocity = Vector2.up * 4;
+        if(Input.GetMouseButtonDown(0)) { // 마우스 왼쪽 버튼을 눌렀을 때
+            rb.velocity = Vector2.up * 4; // velocity = 속도, 위쪽 방향으로 4만큼 속도를 얻는다.
         }
     }
 }
